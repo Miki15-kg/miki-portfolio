344955a76a2e00dbc13032d28a18f0ce8a73690c
Miki15-kg <eeeeeeekiki@gmail.com>
2026-05-28 18:51:00 +0900

    feature/add-header-and-fotter文ランチでpage.tsxを編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import{ MainFotter } from "../components/layout/main/main-footer";
+import{ Component } from "../components/component/component";
+import{ MainFooter } from "../components/layout/main/main-footer";
 import{ MainHeader } from "../components/layout/main/main-header";
 
 export default function Home(){
   return(
     <main>
       <MainHeader />
+      <Component />
         <h1>Hello, World I am Miki Kogoe</h1>
+      <Component />
       <MainFooter />
     </main>
   );
