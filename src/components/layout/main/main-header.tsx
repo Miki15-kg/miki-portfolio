a99c97662200259d94d8df776ba3d03288e0f945
Miki15-kg <eeeeeeekiki@gmail.com>
2026-06-11 18:47:50 +0900

    main-headerを編集

--- src/components/layout/main/main-header.tsx
+++ src/components/layout/main/main-header.tsx
@@ -1,8 +1,8 @@
 export const MainHeader = () =>{
     return(
         <header className='bg-neutral-950 px-6 py-4 text-white'>
-            <div className = 'flex'>
-                <h1 className='font-bold'>Portfolio</h1>
+            <div className = 'flex items-center justify-between'>
+                <h1 className='text-2xl font-bold'>Portfolio</h1>
                 <nav>
                     <a href='/about'>About</a>
                     <a href='/project'>Project</a>
