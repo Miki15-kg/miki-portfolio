77b43f0ec996817971e7ca3a2dbf200cd032ebe1
Miki15-kg <eeeeeeekiki@gmail.com>
2026-06-11 18:30:00 +0900

    main-headerを編集

--- src/components/layout/main/main-header.tsx
+++ src/components/layout/main/main-header.tsx
@@ -1,7 +1,9 @@
 export const MainHeader = () =>{
     return(
-        <header className='bg-neutral-950 text-white'>
-            <h1 className='font-bold text-xl'>Header</h1>
+        <header className='bg-neutral-950 px-6 py-4 text-white'>
+            <div>
+                <h1>Portfolio</h1>
+            </div>
         </header>
     )
 }
