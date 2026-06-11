341b1183e5972c9094516d1047dd235bee010566
Miki15-kg <eeeeeeekiki@gmail.com>
2026-06-11 18:55:13 +0900

    main-footerを編集

--- src/components/layout/main/main-footer.tsx
+++ src/components/layout/main/main-footer.tsx
@@ -1,6 +1,6 @@
 export const MainFooter = () => {
     return(
-        <footer className='text-center text-white'>
+        <footer className='bg-neutral-950 px-6 py-6 text-canter text-white'>
             <h1 className='font-bold text-xl'>Footer</h1>
         </footer>
     );
