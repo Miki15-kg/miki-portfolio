893d9b0011e01aba0b37ba8bcf1b1c96f3bf311b
Miki15-kg <eeeeeeekiki@gmail.com>
2026-06-24 13:20:10 +0900

    feature/add-main-pageでpage.tsxを編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,19 @@ export default function Home() {
           </section>
         
         <section id="skills">
-          <h2>Skills</h2>
-          <div className='mx-auto flex w-full max-w-5xl flex-col gap-16 px-6 py-12'>
-            <div>
-              <h3>Next.js</h3>
-              <p>ページ作成、ルーティング、コンポーネント分割を練習いています。</p>
+          <h2 className='text-2xl font-bold text-neutral-900'>Skills</h2>
+          <div className='mt-6 grid gap-4 md:grid-cols-3'>
+            <div className='rounded-xl border border-neutral-200 bg-white p-6 shadow-sm'>
+              <h3 className='text-xl font-bold text-neutral-900'>Next.js</h3>
+              <p className='mt-3 text-sm leading-6 text-neutral-600'>ページ作成、ルーティング、コンポーネント分割を練習いています。</p>
             </div>
-            <div>
-              <h3>Trailwind CSS</h3>
-              <p>ページ作成、ルーティング、コンポーネント分割を練習しています。</p>
+            <div className='rounded-xl border border-neutral-200 bg-white p-6 shadow-sm'>
+              <h3 className='text-xl font-bold text-neutral-900'>Trailwind CSS</h3>
+              <p className='mt-3 text-sm leading-6 text-neutral-600'>ページ作成、ルーティング、コンポーネント分割を練習しています。</p>
             </div>
-            <div>
-              <h3>GitHub</h3>
-              <p>ブランチ、コミット、push、PR作成を練習しています。</p>
+            <div className='rounded-xl border border-neutral-200 bg-white p-6 shadow-sm'>
+              <h3 className='text-xl fon-bold text-neutral-900'>GitHub</h3>
+              <p className='mt-3 text-sm leading-6 text-neutral-600'>ブランチ、コミット、push、PR作成を練習しています。</p>
             </div>
           </div>
         </section>
